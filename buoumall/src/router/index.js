2b0594be7aa93639fddd69de54bbdce154a78b58
Qkxinq <34433232+Qkxinq@users.noreply.github.com>
2018-01-03 10:03:33 +0800

    Add files via upload
    路由

--- buoumall/src/router/index.js
+++ buoumall/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home'
+import TodayLook from'@/components/todaylook'
+import Shop from '@/components/shop'
 
 Vue.use(Router)
 
@@ -10,6 +12,15 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+    	path: '/todaylook',
+    	component: TodayLook
+    },
+    {
+    	path: '/shop',
+    	component: Shop
     }
+
   ]
 })
